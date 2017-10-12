f794ffb772d359c699de8352a84b4f73a94a48ce
xiaoyishan <xiaoyishan1992@163.com>
2017-10-12 16:00:19 +0800

    add some of layer ivar

--- Expansion(duplicateUI)/Expansion(duplicateUI)/UIView+Duplicate.m
+++ Expansion(duplicateUI)/Expansion(duplicateUI)/UIView+Duplicate.m
@@ -15,7 +15,13 @@
 - (UIView*)duplicate
 {
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    UIView *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
 }
 @end
 
@@ -23,61 +29,130 @@
 @implementation UIButton (Duplicate)
 - (UIButton*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UIButton *NewBtn = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewBtn.layer.masksToBounds = self.layer.masksToBounds;
+    NewBtn.layer.cornerRadius = self.layer.cornerRadius;
+    NewBtn.layer.borderColor = self.layer.borderColor;
+    NewBtn.layer.borderWidth = self.layer.borderWidth;
+    NewBtn.clipsToBounds = self.clipsToBounds;
+    return NewBtn;
+}
 @end
 
 //标签
 @implementation UILabel (Duplicate)
 - (UIView*)duplicate{
+    
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UILabel *NewLabel = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewLabel.layer.masksToBounds = self.layer.masksToBounds;
+    NewLabel.layer.cornerRadius = self.layer.cornerRadius;
+    NewLabel.layer.borderColor = self.layer.borderColor;
+    NewLabel.layer.borderWidth = self.layer.borderWidth;
+    NewLabel.clipsToBounds = self.clipsToBounds;
+    return NewLabel;
+}
 @end
 
 //图片
 @implementation UIImageView (Duplicate)
 - (UIImageView*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UIImageView *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
+}
 @end
 
 //输入框
 @implementation UITextField (Duplicate)
 - (UITextField*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UITextField *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
+}
 @end
 
 //文本框
 @implementation UITextView (Duplicate)
 - (UITextView*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UITextView *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
+    
+}
 @end
 
 //滚动视图
 @implementation UIScrollView (Duplicate)
 - (UIScrollView*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UIScrollView *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
+    
+}
 @end
 
 //表格
 @implementation UITableView (Duplicate)
 - (UITableView*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UITableView *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
+    
+}
 @end
 
 //集合
 @implementation UICollectionView (Duplicate)
 - (UICollectionView*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UICollectionView *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
+    
+}
 @end
 
 //网页
 @implementation UIWebView (Duplicate)
 - (UIWebView*)duplicate{
     NSData * tempArchive = [NSKeyedArchiver archivedDataWithRootObject:self];
-    return [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];}
+    UIWebView *NewView = [NSKeyedUnarchiver unarchiveObjectWithData:tempArchive];
+    NewView.layer.masksToBounds = self.layer.masksToBounds;
+    NewView.layer.cornerRadius = self.layer.cornerRadius;
+    NewView.layer.borderColor = self.layer.borderColor;
+    NewView.layer.borderWidth = self.layer.borderWidth;
+    NewView.clipsToBounds = self.clipsToBounds;
+    return NewView;
+    
+}
 @end
